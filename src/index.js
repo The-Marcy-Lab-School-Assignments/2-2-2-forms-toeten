7aea69960bfd3cb5052b63f6742f83c840c8ec77
toeten <n.santiago0228@gmail.com>
2024-05-16 15:19:45 -0400

    answered q 10

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@
  *
  */
 
-const handleSubmit = () => {
+const handleSubmit = (event) => {
+  event.preventDefault()
+  const form = event.target
+  debugger;
+  const formValues = Object.fromEntries(new FormData(form));
 };
+
+document.querySelector('form').addEventListener('submit', handleSubmit)
