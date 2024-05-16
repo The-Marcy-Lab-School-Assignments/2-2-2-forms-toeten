64f8e9d35aff80bb042764cf4de9590f21131404
toeten <n.santiago0228@gmail.com>
2024-05-16 15:47:37 -0400

    answered q 10 fr

--- src/index.js
+++ src/index.js
@@ -7,6 +7,12 @@ const handleSubmit = (event) => {
   const form = event.target
   debugger;
   const formValues = Object.fromEntries(new FormData(form));
+  console.log(formValues)
+  document.getElementById("results-username").textContent = formValues.username
+  document.getElementById("results-coding-level").textContent = formValues.codingLevel
+  document.getElementById("results-location").textContent = formValues.location
+  document.getElementById("results-did-like-assignment").textContent = formValues.didLikeAssignment ? "Yes" : "No"
+
 
   form.reset();
 };
